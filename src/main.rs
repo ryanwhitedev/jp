dbe5d56b6a9e8f45c15f94d2e66a7e7f138b7cd3
Ryan White <ryanwhite92@protonmail.com>
2024-02-24 11:32:57 -0800

    Accept input from file or stdin

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
+use std::fs::File;
+use std::io::{self, IsTerminal, Read};
+
 use jp::parse;
 
-fn main() {
-    let json = r#"{
-        "key": "value",
-        "int": 42,
-        "float": -3.14,
-        "bool_true": true,
-        "bool_false": false,
-        "null_type": null,
-        "empty_array": [],
-        "array": ["one", "two"],
-        "empty_object": {},
-        "object": {
-            "int": 42
-        }
-    }"#;
+const USAGE: &str = "Usage: jp [FILE]";
 
-    if let Err(e) = parse(&json) {
+fn print_usage() {
+    println!("{}", USAGE);
+    std::process::exit(0);
+}
+
+fn main() {
+    let mut buffer = String::new();
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    if let Some(filename) = args.get(0) {
+        let mut file = match File::open(filename) {
+            Ok(f) => f,
+            Err(_) => {
+                eprintln!("jp: {}: No such file or directory", filename);
+                std::process::exit(1);
+            }
+        };
+        file.read_to_string(&mut buffer).unwrap_or_else(|e| {
+            eprintln!("Error reading from file {}: {}", filename, e);
+            std::process::exit(1);
+        });
+    } else if !io::stdin().is_terminal() {
+        // Allow piped input via stdin
+        io::stdin().read_to_string(&mut buffer).unwrap_or_else(|e| {
+            eprintln!("Error reading from stdin: {}", e);
+            std::process::exit(1);
+        });
+    } else {
+        print_usage();
+    }
+
+    if let Err(e) = parse(&buffer) {
         eprintln!("Invalid JSON: {}", e);
+        std::process::exit(1);
     }
 }
